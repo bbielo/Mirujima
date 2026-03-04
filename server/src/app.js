3ac360c15fe502fb9da9eeb27b1ae0817f65b983
bbielo <eulbi90@gmail.com>
2026-03-04 16:51:45 +0900

    fix: modify middleware in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -20,7 +20,13 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // 전역 미들웨어
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "hhttps://mirujima.onrender.com"
+    ],
+    credentials: true,
+}));
 app.use(express.json());
 
 // 라우터
